4ddca3953d4d2b5146ae8f48122476e370494236
Reytinaz <akakijvesnuskin@gmail.com>
2024-08-18 16:43:11 +0300

    1

--- .bot-host/main.py
+++ .bot-host/main.py
@@ -1,5 +1,8 @@
 import os
+import shutil
 
 print("hello world")
-debug_File1 = open("debug_1", "w")
+debug_File1 = open("debug_1", "w+")
 debug_File1.write("hello world")
+debug_File1.close()
+shutil.move("debug_1", "_debug")
