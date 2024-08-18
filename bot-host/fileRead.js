7b95b9f4889ecbc076ca2b53697b55597cdc8238
Reytinaz <akakijvesnuskin@gmail.com>
2024-08-18 17:33:51 +0300

    4

--- bot-host/fileRead.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    var fileInput = document.getElementById('fileInput');
-
-    fileInput.addEventListener('change', function(e) {
-      // Put the rest of the demo code here.
-    });
-}
